5ff17870d43fdb7635a00e403eace742f37d9870
Alwyn Durham <alwyn.durham@evisions.com>
2017-11-17 10:27:41 -0800

    Added some pesudocode thoughts to the js file & a button

---
+++ assets/javascript/campaign.js
@@ -0,0 +1,26 @@
+
+
+// Get user's location information
+  // State
+  // City
+  // More local?  Not sure what affects elections.
+  // Specific address affects school districts so we might need that.
+
+// Validate the location info
+// If it's bad, return an error
+
+// If it's good, get the list of upcoming elections
+// It would be nice to have them sorted and labelled for us, state, and local elections
+
+// For each election, clicking on it should return the list of candidates.
+
+// Under each candidate we want to show where they're getting their $
+// User shouldn't have to open a modal, we want it all on the screen at once so they
+// can compare side by side with minimal clicking.
+
+$("#get-info").on("click", function() {
+
+
+
+
+});
